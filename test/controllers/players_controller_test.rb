265ff485675401f90b1afb239545d31fae70661a
bradzara <bradzara93@gmail.com>
2024-06-04 11:11:12 -0700

    Add index test controller

--- test/controllers/players_controller_test.rb
+++ test/controllers/players_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class PlayersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/players.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Player.count, data.length
+  end
 end
