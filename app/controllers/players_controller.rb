50472f3da57fd9cfb7e9d401d12761dbc6daa0d4
bradzara <bradzara93@gmail.com>
2024-06-05 11:35:37 -0700

    Add update action

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -10,7 +10,19 @@ class PlayersController < ApplicationController
   end
 
   def create
-    @player = player.create(
+    @player = Player.create(
+      name: params[:name],
+      team: params[:team],
+      position: params[:position],
+      number: params[:number],
+      image_url: params[:image_url]
+    )
+    render :show
+  end
+
+  def update
+    @player = Player.find_by(id: params[:id])
+    @player.update(
       name: params[:name],
       team: params[:team],
       position: params[:position],
